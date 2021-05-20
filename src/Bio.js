7ba1efaa0f99585adbe256e9c833b78f0b0cb01a
Oussama Saoudi <oussama.saoudi@ibm.com>
2021-05-20 08:18:13 -0400

    Preliminary population of Home tab

--- src/Bio.js
+++ src/Bio.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Bio = () => {
   return (
     <div>
-      <h2>Here is where my bio will be</h2>
+      <h3>Nice to Meet You.</h3>
+      <p>
+        I am a Computer Science student at UBC. I conduct
+        research in AI. I also have a blog you might enjoy.
+        Get in touch with me here.
+      </p>
     </div>
   )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
 const Projects = () => {
+  const projectList = [
+    {
+      title: 'Orate-io'
+    },
+    {
+      title: 'Labyrinth'
+    },
+    {
+      title: 'Personal Website'
+    },
+    {
+      title: 'OpenJ9'
+    },
+    {
+      title: 'Autonomous Vehicles'
+    }
+  ]
   return (
     <div>
-      <h3>This is where my projects will be!</h3>
+      <h3>Here&apos;s some of my work:</h3>
+      <ul>
+        {
+          projectList.map((project, index) =>
+            <li key={index}>{project.title}</li>
+          )
+        }
+      </ul>
     </div>
   )
 }
