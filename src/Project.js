24cbef9fe6f821639e3a76b815726e1fc966236c
Oussama Saoudi <oussama.sa216@gmail.com>
2021-05-22 11:43:54 -0400

    Added elementary card style and background color

--- src/Project.js
+++ src/Project.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-const Project = ({ title, content }) => {
+const Project = ({ title, content, tags }) => {
   return (
-    <div className='container'>
-      <h4>{title}</h4>
-      <br />
-      {content}
+    <div className='sm:my-6 sm:mx-4 md:mx-8 lg:ms-12'>
+      <div className='w-full px-4 pb-5 sm:px-6 md:px-8 sm:rounded-xl bg-white sm:shadow-md'>
+        <div className="uppercase tracking-wide text-lg  pt-5 text-purple-700 font-bold">{title}</div>
+        <div className="text-justify text-gray-600">{content}</div>
+        <div className='pt-2'>
+          {tags.map((tag,index) => (
+            <span key={index} className='inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded'>
+              {tag}
+            </span>
+          )
+          )}
+        </div>
+      </div>
     </div>
   )
 }
 Project.propTypes = {
   title: PropTypes.string.isRequired,
-  content: PropTypes.string
+  content: PropTypes.string.isRequired,
+  tags: PropTypes.array.isRequired
 }
 export default Project
