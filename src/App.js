247444a853b3353834eed4bd24f47aa44c2e4437
Oussama Saoudi <oussama.sa216@gmail.com>
2021-05-23 08:49:54 -0400

    fixed side navbar to be full screen

--- src/App.js
+++ src/App.js
@@ -10,28 +10,10 @@ import Blog from './Blog'
 
 function App() {
   return (
-    <div className='flex bg-gray-900'>
-      <div className='flex md:flex-row flex-navbar md:min-h-screen'>
-        <div className="flex-col md:w-20 text-gray-700 topography" x-data='{ open: false}'>
-          <div className=''>
-          </div>
-          <nav className='sticky top-0 flex md:block md:overflow-y-auto'>
-            <div className="block bg-amber-300 py-5 py-4 uppercase tracking-wide text-white font-bold">
-              Link1
-            </div>
-            <span className="block py-5 py-4 uppercase tracking-wide text-white font-bold">
-              Link2
-            </span>
-            <span className="block hover:bg-purple-400 py-5 py-4 uppercase tracking-wide text-white font-bold">
-              Link3
-            </span>
-
-          </nav>
-        </div>
-      </div>
-      <div>
-        <Router>
-          <Navbar />
+      <Router>
+        <div className='flex bg-gray-900'>
+        <Navbar />
+        <div>
           <Switch>
             <Route path='/blog'>
               <Blog />
@@ -40,9 +22,9 @@ function App() {
               <Home />
             </Route>
           </Switch>
-        </Router>
-      </div>
-    </div>
+        </div>
+        </div>
+      </Router>
 
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,9 +6,25 @@ import {
 const Navbar = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/users">Get in Touch</Link>
+      <div className='md:flex md:flex-row flex-navbar h-full'>
+        <div className="md:w-20 flex-col text-gray-700 topography" x-data='{ open: false}'>
+          <nav className='steicky top-0 md:block'>
+
+            <div className="block bg-amber-300 py-5 py-4 uppercase tracking-wide text-white font-bold">
+              <Link to="/">Home</Link>
+            </div>
+
+            <span className="block hover:bg-purple-400 py-5 py-4 uppercase tracking-wide text-white font-bold">
+              <Link to="/blog">Blog</Link>
+            </span>
+
+            <span className="block hover:bg-purple-400 py-5 py-4 uppercase tracking-wide text-white font-bold">
+              <Link to="/users">Get in Touch</Link>
+            </span>
+
+          </nav>
+        </div>
+      </div>
     </div>
   )
 }
